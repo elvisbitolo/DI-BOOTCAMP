d7a3f351daa5880e488ebe8c89952be4ad17f2c2
Elvo <elvisbitolo11@gmail.com>
2026-03-25 12:47:57 +0300

    week 2 exercises

---
+++ day-1 daily-challenge.py
@@ -0,0 +1,55 @@
+class Farm:
+    def __init__(self, farm_name):
+        self.name = farm_name
+        self.animals = {}
+
+    def add_animal(self, animal_type=None, count=1, **kwargs):
+        if animal_type:
+            if animal_type in self.animals:
+                self.animals[animal_type] += count
+            else:
+                self.animals[animal_type] = count
+
+        for animal, qty in kwargs.items():
+            if animal in self.animals:
+                self.animals[animal] += qty
+            else:
+                self.animals[animal] = qty
+
+    def get_info(self):
+        result = f"{self.name}'s farm\n\n"
+        for animal, count in self.animals.items():
+            result += f"{animal:<6} : {count}\n"
+        result += "\n    E-I-E-I-0!"
+        return result
+
+    def get_animal_types(self):
+        return sorted(self.animals.keys())
+
+    def get_short_info(self):
+        animal_list = []
+        for animal in self.get_animal_types():
+            if self.animals[animal] > 1:
+                animal_list.append(animal + "s")
+            else:
+                animal_list.append(animal)
+
+        if len(animal_list) > 1:
+            animals_str = ", ".join(animal_list[:-1]) + " and " + animal_list[-1]
+        else:
+            animals_str = animal_list[0]
+
+        return f"{self.name}'s farm has {animals_str}."
+
+
+# TEST
+macdonald = Farm("McDonald")
+
+macdonald.add_animal('cow', 5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal(goat=12, duck=2)
+
+print(macdonald.get_info())
+print()
+print(macdonald.get_short_info())
